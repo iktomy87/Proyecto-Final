0739306f10ccd91f0403d9daf881373923a07793
ceso217 <baronicecilio@gmail.com>
2024-10-12 17:44:32 -0300

    Śección gestión de artículos y cambiar contraseña creada y retoques funcionalidad comentarios

--- blogging/urls.py
+++ blogging/urls.py
@@ -3,7 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -14,11 +13,19 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('eliminarComentario/<int:id>/', views.eliminar_comentario, name='eliminar_comentario'),
+    path('comentarios/actualizar/<int:id>/', views.actualizar_comentario, name='actualizar_comentario'),
     path('busqueda/', views.blog_busqueda, name='blog_busqueda'),
-    path("acerca-de/", TemplateView.as_view(template_name="acerca_de.html"), name="acerca_de"),
+    path("acerca-de/", views.acerca_de, name="acerca_de"),
     path("contacto/", views.blog_contact, name="blog_contact"),
     path('filtros/', views.filtros, name='filtros'),
     path('get_weather/<str:lat>/<str:lon>/', views.get_weather, name='get_weather'),
+    path('gestionArticulos/', views.gestion_articulos, name='gestion_articulos'),
+    path('crearArticulo/', views.crear_articulo, name='crear_articulo'),
+    path('eliminarArticulo/<int:id>', views.eliminar_articulo, name='eliminar_articulo'),
+    path('edicionArticulo/<int:id>', views.edicion_articulo, name="edicion_articulo"),
+    path('editarArticulo/', views.editar_articulo, name='editar_articulo'),
+    path('change_password/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html')),
+    path('change_password/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
